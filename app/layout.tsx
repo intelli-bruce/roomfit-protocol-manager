c86c0d4b1853dedd5a8a52fce16dd61b2b2acbe8
bruce <bruce@intellieffect.com>
2025-03-05 13:34:13 +0900

    Simplify layout and remove unused assets.
    Removed custom fonts, static assets, and complex styling from the layout and page files. Streamlined components for simplicity and removed redundant dependencies for a cleaner structure.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
 import "./globals.css";
+import React from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,17 +9,16 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <body
+    >
+    {children}
+    </body>
     </html>
   );
 }
